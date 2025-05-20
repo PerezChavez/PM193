db20568aa5fab5bb774e5f6686bafbe2e311dd9d
Carol Chavez <122042076@upq.edu.mx>
2025-05-19 18:06:40 -0600

    practica1

--- Practica1/practica1.js
+++ Practica1/practica1.js
@@ -8,4 +8,24 @@ const saludoo = "Holii, " + nombre + ". Tienes " + edad + " años";
 
 console.log(saludoo);
 
+//Ejercicio 1 Parte 2
+
+function cuadrado (numero) {
+    return numero * numero;
+}
+
+console.log(cuadrado(2)); 
+console.log(cuadrado(5)); 
+console.log(cuadrado(7)); 
+
+//Ejercicio 1 parte 3
+
+const saludoPersonalizado = (nombre, edad) => {
+  return `Hola, me llamo ${nombre} y tengo ${edad} años.`;
+};
+
+const nombre1 = prompt("Ingresa tu nombre:");
+const edad1 = prompt("Ingresa tu edad:");
+
+alert(saludoPersonalizado(nombre1, edad1));
 

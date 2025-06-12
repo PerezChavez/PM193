5a449d34cacf3dd385491c6c5e4d614095996edd
Carol Chavez <122042076@upq.edu.mx>
2025-06-12 17:04:04 -0600

    UseState en botón

--- my-app2/App.js
+++ my-app2/App.js
@@ -1,32 +1,33 @@
 /*Zona 1 : Importaciones */
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
-import React, {useState} from 'react'; //Permite manipular el estado de los componentes 
+import React, {useState} from 'react'; 
 
 
-//componente Propio Texto 
-const Texto = () => {
-  const [contenido, setContenido] = useState('Hola Mundo');
-  const actualizaTexto = () => {
-    setContenido('Estado modificado');
-  }
+// Componente Propio Texto 
+const Texto = ({ contenido }) => {
   return (
-    <Text onPress={actualizaTexto}>{contenido}</Text>
+    <Text>{contenido}</Text>
   );
 }
+
 
 /*Zona 2 : Main o zona principal*/
 
 export default function App() {
+  const [contenido, setContenido] = useState('Hola Mundo');
+
+  const actualizaTexto = () => {
+    setContenido('Estado modificado');
+  }
+
   return (
-
     <View style={styles.container}>
-      <Texto >Hola</Texto>
-      <Texto >Mundo</Texto>
-      <Texto>ReactNative</Texto>
-      <Button title='Presionar'></Button>
+      <Texto contenido={contenido} />
+      <Texto contenido={contenido} />
+      <Texto contenido={contenido} />
+      <Button title='Presionar' onPress={actualizaTexto} />
       <StatusBar style="auto" />
-
     </View>
   );
 }

8b597c16720a93bd73ca9367248c0334e0d71e33
Carol Chavez <122042076@upq.edu.mx>
2025-06-17 17:19:58 -0600

    Practica 6 StyleSheet & Flex

--- my-app2/App.js
+++ my-app2/App.js
@@ -3,17 +3,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import React, {useState} from 'react'; 
 
-
 // Componente Propio Texto 
-const Texto = ({ contenido }) => {
+const Texto = ({ style, contenido }) => {
   return (
-    <Text>{contenido}</Text>
+    <Text style={[styles.text, style]}>{contenido}</Text>
   );
 }
 
-
 /*Zona 2 : Main o zona principal*/
-
 export default function App() {
   const [contenido, setContenido] = useState('Hola Mundo');
 
@@ -23,15 +20,15 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Texto contenido={contenido} />
-      <Texto contenido={contenido} />
-      <Texto contenido={contenido} />
+      <Texto style={styles.amarillo} contenido={contenido} />
+      <Texto style={styles.rojo} contenido={contenido} />
+      <Texto style={styles.verde} contenido={contenido} />
+      
       <Button title='Presionar' onPress={actualizaTexto} />
       <StatusBar style="auto" />
     </View>
   );
 }
-
 
 /*Zona 3 : Estilos - Estetica */
 const styles = StyleSheet.create({
@@ -39,6 +36,15 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
+    flexDirection: 'space-between',
   },
+  text: {
+    fontSize: 28,
+    textAlign: 'center',
+    padding: 10,
+  },
+  amarillo: { backgroundColor: 'yellow' }, 
+  rojo: { backgroundColor: 'red'},
+  verde: { backgroundColor: 'green'},
 });

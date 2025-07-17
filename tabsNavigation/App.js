2139c5988d69f06fad499512f9602dca8334374d
Carol Chavez <122042076@upq.edu.mx>
2025-07-17 16:21:01 -0600

    Practica 17

--- tabsNavigation/App.js
+++ tabsNavigation/App.js
@@ -1,10 +1,11 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Ionicons } from '@expo/vector-icons';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
 import Home from './screens/home';
-import Profile from './screens/profile';
 import Settings from './screens/settings';
+import PerfilStack from './screens/perfil';
 
 const Tab = createBottomTabNavigator();
 
@@ -14,29 +15,25 @@ export default function App() {
       <Tab.Navigator
         initialRouteName="Home"
         screenOptions={({ route }) => ({
-          headerShown: false,
+          headerShown: true,
           tabBarIcon: ({ color, size }) => {
             let iconName;
-            
             if (route.name === 'Home') {
-              iconName = 'home';
+              iconName = 'home-outline';
             } else if (route.name === 'Profile') {
-              iconName = 'person';
+              iconName = 'person-outline';
             } else if (route.name === 'Settings') {
-              iconName = 'settings';
+              iconName = 'settings-outline';
             }
             return <Ionicons name={iconName} size={size} color={color} />;
           },
           tabBarActiveTintColor: '#007BFF',
           tabBarInactiveTintColor: 'gray',
-          tabBarStyle: {
-            paddingBottom: 5,
-            height: 60,
-          },
+          tabBarStyle: { height: 60 },
         })}
       >
         <Tab.Screen name="Home" component={Home} />
-        <Tab.Screen name="Profile" component={Profile} />
+        <Tab.Screen name="Profile" component={PerfilStack} />
         <Tab.Screen name="Settings" component={Settings} />
       </Tab.Navigator>
     </NavigationContainer>
